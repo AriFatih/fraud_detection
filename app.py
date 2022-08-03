8909ca653ceff089ca773c3a789b6df23a954f6a
AriFatih <69502743+AriFatih@users.noreply.github.com>
2022-08-03 06:26:59 +0200

    rounding the decimals

--- app.py
+++ app.py
@@ -64,7 +64,8 @@ if uploaded_file is not None:
     proba_xgb = model_xgb.predict_proba(X)
     df["pred_proba_log"] = proba_log[:,1]
     df["pred_proba_xgb"] = proba_xgb[:,1]
-    df = df.round({"pred_proba_log": 2, "pred_proba_xgb": 2})
+    df["pred_proba_log"] = df["pred_proba_log"].round(2)
+    df["pred_proba_xgb"] = df["pred_proba_xgb"].round(2)
     df = df.sort_values(by='class', ascending=False)
     #st.write("The number of ")
     st.write(df)
